5c081f1d0a686706c088aacc3049e2ceb7c7311b
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-04 03:11:01 +0600

    fake data and custom hook created

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
 import './Reviews.css';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useReviews();
     return (
         <div>
             <h2>Reviews Section</h2>
+            {
+                reviews?.slice(0, 3)?.map(review => console.log(review))
+            }
         </div>
     );
 };
