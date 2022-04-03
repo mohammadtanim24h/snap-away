a311932e1b39298da045ea9e5f92c2c1b510c824
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-04 04:01:53 +0600

    review added

---
+++ src/components/Review/Review.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Review.css";
+
+const Review = ({ review }) => {
+    const { img, name, rating, review: userReview } = review;
+    return (
+        <div className="user-review shadow">
+            <div className="reviewer-img">
+                <img src={img} alt="" />
+            </div>
+            <div className="reviewer-details">
+                <h3 className="text-secondary">{name}</h3>
+                <p className="lead">{userReview}</p>
+                <p className="lead">Rating: {rating}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
 import './Reviews.css';
 
 const Reviews = () => {
     const [reviews, setReviews] = useReviews();
     return (
-        <div>
-            <h2>Reviews Section</h2>
-            {
-                reviews?.slice(0, 3)?.map(review => console.log(review))
-            }
+        <div className='container'>
+            <div className='reviews-container'>
+                {
+                    reviews.map(review => <Review key={review.id} review={review}></Review>)
+                }
+            </div>
         </div>
     );
 };
