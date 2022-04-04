2c0d8221d8a7838f1421c7eb20554e52c5904d2d
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-04 21:20:00 +0600

    title alignment fixed

--- src/components/RevenueAreaChart/RevenueAreaChart.js
+++ src/components/RevenueAreaChart/RevenueAreaChart.js
@@ -6,7 +6,7 @@ const RevenueAreaChart = () => {
     const chart = useChart();
     return (
         <div>
-            <h2 className="text-secondary chart-title">Investment Vs Revenue</h2>
+            <h2 className="text-secondary text-center">Investment Vs Revenue</h2>
             <AreaChart width={500} height={400} data={chart}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <Area type="monotone" dataKey="investment" stroke="#8884d8" fill="#8884d8" />
